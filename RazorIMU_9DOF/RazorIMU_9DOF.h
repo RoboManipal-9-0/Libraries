21517177d8710d6a3e6c7b5c21db233cfd0f87a1
TheProjectsGuy <123avneesh@gmail.com>
2018-09-26 20:15:16 +0530

    Added basic code for 9DOF IMU, issue #5 addressed

--- RazorIMU_9DOF/RazorIMU_9DOF.h
+++ RazorIMU_9DOF/RazorIMU_9DOF.h
@@ -21,9 +21,35 @@
 
 // Serial method of connection
 class RazorIMU_9DOF {
+    // Raw values of Pitch, Roll and Yaw as received from the sensors
     float PRY_raw_values[3];
+    float PRY_full_scale[3];   // 0 to 360 degree values
+    float PRY_full_scale_ref[3];  // Reference values of Pitch, Roll and Yaw
+    void Calculate_fullScales();
+    // Initialize IMU - Initialize the reference values
+    void GrabReference();
+    // Update the raw values from the serial
+    void GrabData();
+    // Serial line to which the IMU is attached
     Stream *IMU_Serial;
-
+public:
+    // ##################   Constructors   ######################
+    RazorIMU_9DOF();
+    RazorIMU_9DOF(Stream *AttachedSerial);
+    // ###############  Public member functions   ################
+    // Attach IMU to serial
+    void AttachIMUSerial(Stream *AttachedSerial);
+    // Parse data from serial (raw) - Receive and store it
+    void UpdateData();
+    // #############   Retrieving data from object   ############
+    // Retrieving raw PRY values
+    float GetRaw_PITCH();
+    float GetRaw_ROLL();
+    float GetRaw_YAW();
+    // Retrieving full scale RPY values
+    float GetRoll();
+    float GetPitch();
+    float GetYaw();
 };
 
 // End of guard code
