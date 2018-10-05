e879f60daaeab9df11836e43bc8326767a0c8fb0
TheProjectsGuy <123avneesh@gmail.com>
2018-10-05 07:58:33 +0530

    Added basic angles code

--- NWCHBase/NWCHBase.cpp
+++ NWCHBase/NWCHBase.cpp
@@ -7,6 +7,26 @@
 // Including the library
 #include "NWCHBase.h"
 
+// Constructors
+NWCHBase::NWCHBase() {
+    // Empty constructor
+}
+NWCHBase::NWCHBase(int num_wheels) {
+    
+}
+
+// ############## Initializer functions #####################
+void NWCHBase::InitializeWheels(int num_wheels) {
+    // Set the number of wheels
+    this->setNumberOfWheelsTo(num_wheels);
+}
+void NWCHBase::InitializeWheels(int *angles_deg) {
+    String msg = "Initializing angle configuration to ";
+    for (int i = 0; i < this->NUMBER_OF_WHEELS; i ++) {
+        this->angles[i] = angles_deg[i] * DEG_TO_RAD;    // Convert angles to radians and assign them
+    }
+}
+
 void NWCHBase::Move_PWM_Angle(int PWM, float angle, float w) {
     // TODO: Implement code for motion of NWCHBase 
 }
